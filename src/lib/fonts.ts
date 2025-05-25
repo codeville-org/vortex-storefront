30d35ae6bc62943f0abdc42dedf1b47c38b5c494
iamVihanga <vihangarashansilva@gmail.com>
2025-05-25 16:40:14 +0530

    Add: Shadcn Initialization

---
+++ src/lib/fonts.ts
@@ -0,0 +1,11 @@
+import { Space_Grotesk, Syne } from "next/font/google";
+
+export const fontSans = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-sans"
+});
+
+export const fontHeading = Syne({
+  subsets: ["latin"],
+  variable: "--font-heading"
+});
